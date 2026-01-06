104ff61a2789f9dfb4b868218ada78aab9f0a3b7
Your Name <you@example.com>
2026-01-06 11:57:49 +0530

    commit

---
+++ projects.js
@@ -0,0 +1,4 @@
+// In your script.js handleHover function, add '.project-card'
+const interactables = document.querySelectorAll(
+    "a, button, .logo, .skill-card, .contact-card, .icon-link, .project-card"
+);
